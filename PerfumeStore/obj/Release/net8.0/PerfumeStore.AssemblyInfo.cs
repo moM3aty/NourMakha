fd9dde2a28442b6196607ead382e9139829f07b6
moM3aty <mo.m3aty@gmail.com>
2026-02-20 16:58:44 +0200

    NourMakha

--- PerfumeStore/obj/Release/net8.0/PerfumeStore.AssemblyInfo.cs
+++ PerfumeStore/obj/Release/net8.0/PerfumeStore.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("PerfumeStore")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+80d9213ba3e3c897dbe693e982db6991ef874181")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+59e81767d3761c801ca50ad7c91e33fae7cb711c")]
 [assembly: System.Reflection.AssemblyProductAttribute("PerfumeStore")]
 [assembly: System.Reflection.AssemblyTitleAttribute("PerfumeStore")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
